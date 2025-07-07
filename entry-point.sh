117c14c7dd1f90dba0f05bab76d20b974be21a4c
Jipson george <140504207+Jipson-George@users.noreply.github.com>
2025-07-07 21:12:20 +0530

    modification for railway

--- entry-point.sh
+++ entry-point.sh
@@ -16,5 +16,11 @@ if [ ! -f books/models.py ]; then
   python manage.py inspectdb > book/models.py
 fi
 
+# ✅ Run SQL seed script only if a marker doesn't exist
+if [ ! -f .seeded ]; then
+  echo "Seeding database from init.sql..."
+  PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -U $POSTGRES_USER -d $POSTGRES_DB -f init.sql && touch .seeded
+fi
+echo "Starting Gunicorn..."
 gunicorn GutenbergDjango.wsgi:application --bind 0.0.0.0:8000
 exec
