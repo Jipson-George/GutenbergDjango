1172f01ac119fb2f52249fb7bfb646ab0ad62c11
Jipson george <140504207+Jipson-George@users.noreply.github.com>
2025-07-07 22:33:42 +0530

    Update entry-point.sh

--- entry-point.sh
+++ entry-point.sh
@@ -27,10 +27,10 @@ if [ ! -f books/models.py ]; then
 fi
 
 # ✅ Run SQL seed script only if a marker doesn't exist
-if [ ! -f .seeded ]; then
-  echo "Seeding database from init.sql..."
-  PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -U $POSTGRES_USER -d $POSTGRES_DB -f init.sql && touch .seeded
-fi
+# if [ ! -f .seeded ]; then
+#   echo "Seeding database from init.sql..."
+#   PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -U $POSTGRES_USER -d $POSTGRES_DB -f init.sql && touch .seeded
+# fi
 echo "Starting Gunicorn..."
 gunicorn GutenbergDjango.wsgi:application --bind 0.0.0.0:8000
 exec
