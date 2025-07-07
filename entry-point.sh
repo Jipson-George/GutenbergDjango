92d9a912a1e92299c59977ff66daf05669189598
Jipson george <140504207+Jipson-George@users.noreply.github.com>
2025-07-07 22:48:15 +0530

    Update entry-point.sh

--- entry-point.sh
+++ entry-point.sh
@@ -16,21 +16,21 @@ done
 
 echo "PostgreSQL is up."
 
-python manage.py makemigrations
-# Continue your tasks...
-python manage.py migrate
-echo "Loading seed data from init.sql..."
+
 
 if [ ! -f books/models.py ]; then
   echo "Generating models with inspectdb..."
   python manage.py inspectdb > book/models.py
 fi
+python manage.py makemigrations
+# Continue your tasks...
+python manage.py migrate
+echo "Loading seed data from init.sql..."
 
-✅ Run SQL seed script only if a marker doesn't exist
-if [ ! -f .seeded ]; then
-  echo "Seeding database from init.sql..."
-  PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -U $POSTGRES_USER -d $POSTGRES_DB -f init.sql && touch .seeded
-fi
+# if [ ! -f .seeded ]; then
+#   echo "Seeding database from init.sql..."
+#   PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -U $POSTGRES_USER -d $POSTGRES_DB -f init.sql && touch .seeded
+# fi
 echo "Starting Gunicorn..."
 gunicorn GutenbergDjango.wsgi:application --bind 0.0.0.0:8000
 exec
