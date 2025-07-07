5cbbd5841caf0f056583a3d5e576c6b970494c4f
Jipson george <140504207+Jipson-George@users.noreply.github.com>
2025-07-07 21:18:52 +0530

    check db connection

--- entry-point.sh
+++ entry-point.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 echo "Waiting for PostgreSQL..."
-
-until python -c "import socket; socket.create_connection(('${POSTGRES_HOST}', ${POSTGRES_PORT}), timeout=1)" 2>/dev/null; do
-  sleep 1
+# TEMP DEBUG: print vars
+echo "POSTGRES_USER=$POSTGRES_USER"
+echo "POSTGRES_PASSWORD=$POSTGRES_PASSWORD"
+echo "POSTGRES_DB=$POSTGRES_DB"
+# until python -c "import socket; socket.create_connection(('${POSTGRES_HOST}', ${POSTGRES_PORT}), timeout=1)" 2>/dev/null; do
+#   sleep 1
+# done
+until pg_isready -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -U "$POSTGRES_USER"; do
+  echo "Waiting for database..."
+  sleep 2
 done
+
 
 echo "PostgreSQL is up."
 
