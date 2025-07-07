f5281b7a0808b0c45ff3ca1768508d4d83b03435
Jipson george <140504207+Jipson-George@users.noreply.github.com>
2025-07-07 20:55:31 +0530

    railwaydeployment modifications

--- GutenbergDjango/settings.py
+++ GutenbergDjango/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -118,14 +119,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ALLOWED_HOSTS = ['*']
+
+
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gutenberg_db',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',   # docker service name
-        'PORT': 5432,
-    }
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
--- entry-point.sh
+++ entry-point.sh
@@ -2,7 +2,7 @@
 
 echo "Waiting for PostgreSQL..."
 
-until python -c "import socket; socket.create_connection(('db', 5432), timeout=1)" 2>/dev/null; do
+until python -c "import socket; socket.create_connection(('${POSTGRES_HOST}', ${POSTGRES_PORT}), timeout=1)" 2>/dev/null; do
   sleep 1
 done
 
@@ -16,4 +16,5 @@ if [ ! -f books/models.py ]; then
   python manage.py inspectdb > book/models.py
 fi
 
-python manage.py runserver 0.0.0.0:8000
+gunicorn GutenbergDjango.wsgi:application --bind 0.0.0.0:8000
+exec
