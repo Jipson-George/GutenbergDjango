4bd4818bcb4f6bc54a130f25200d2df4affe6bda
Jipson george <140504207+Jipson-George@users.noreply.github.com>
2025-07-14 17:35:28 +0530

    Update settings.py

--- GutenbergDjango/settings.py
+++ GutenbergDjango/settings.py
@@ -125,7 +125,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ALLOWED_HOSTS = ['*']
 
-
+# In your Django settings
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    # Add your production domains
+]
 
 DATABASES = {
     'default': {
